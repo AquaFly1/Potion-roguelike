bed24748b56be14e65fb916c50555f088074358a
AquaFly1 <louis.emery@hotmail.com>
2025-10-18 20:21:14 -0400

    enemies in 3d

--- Scripts/interaction/interaction_area.gd
+++ Scripts/interaction/interaction_area.gd
@@ -4,6 +4,10 @@ extends Area3D
 
 var next_interaction = null
 
+@export var enemies_num = 3
+@onready var enemies_parent: Node3D = $enemies_parent
+@export var goblin_scene: PackedScene
+
 func _ready() -> void:
 	pkd_interaction_str = []
 	for i in range(len(interactions)):
@@ -16,7 +20,13 @@ func choose_next_interaction():
 func _on_body_entered(body: Node3D) -> void:
 	if body.name == "Player":
 		if interactions != []:
-			call_deferred("change_scene",next_interaction)
+			var pos = -0.4
+			for i in range(enemies_num):
+				var enemy_inst = goblin_scene.instantiate()
+				enemies_parent.add_child(enemy_inst)
+				enemy_inst.position.x += pos
+				pos += 0.4
+#			call_deferred("change_scene",next_interaction)
 
 func change_scene(scene: String) -> void:
 	get_tree().change_scene_to_file(scene)

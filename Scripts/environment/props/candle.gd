6451539fb48560ff0cb2c39a0dfbf21ae20bbbef
AquaFly1 <louis.emery@hotmail.com>
2025-10-26 14:17:59 -0400

    asd

--- Scripts/environment/props/candle.gd
+++ Scripts/environment/props/candle.gd
@@ -2,11 +2,14 @@ extends Node
 
 @onready var armature: Node3D = $StaticBody3D/candle/Armature
 @onready var point: OmniLight3D = $StaticBody3D/candle/Point
+@onready var animation_player: AnimationPlayer = $StaticBody3D/candle/AnimationPlayer
 
 
 func _ready() -> void:
 	armature.visible = false
 	point.visible = false
+	animation_player.autoplay = "0"
+	
 
 
 func light_candle():

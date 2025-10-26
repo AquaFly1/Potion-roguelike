bb9c9dbceb587421515d0a60429c04ad6af06c83
AquaFly1 <louis.emery@hotmail.com>
2025-10-26 14:39:09 -0400

    Update character_controller.gd

--- Scripts/entities/player/character_controller.gd
+++ Scripts/entities/player/character_controller.gd
@@ -115,6 +115,8 @@ func _physics_process(delta):
 	move_and_slide()
 
 func _input(event: InputEvent) -> void:
+	
 	if event.is_action_pressed("interact"):
 		if candle_look:
+			print(event.as_text())
 			candle_look.light_candle()

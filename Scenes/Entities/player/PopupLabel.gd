86ef7ab676b7db3d4c03bb22a9b00738495944cf
Ti-Die <ewm.hoover@gmail.com>
2025-10-27 23:45:43 -0400

    euh... pas les interactions mais le texte est cool

---
+++ Scenes/Entities/player/PopupLabel.gd
@@ -0,0 +1,58 @@
+extends Label
+
+var interact_text: String = ""
+var text_tween: Tween
+var erase_tween: Tween
+@onready var interact_ray_active: int = 0
+@export var letters_per_sec: float
+var anim_time: float = 20
+var is_exit: bool
+
+
+
+
+func add_text(input_text) -> void:
+	interact_text = input_text
+		
+	#tally how many objects say theyre being viewed, value is basically always 0 or 1 but
+	#when the viewd object switches it might fuck up a bool if the exit call happens
+	#after teh new object's enter call
+	interact_ray_active += 1
+	
+	#basically as long as the text isnt already there, avoid erasing just to put the
+	#same thing again
+	if text != interact_text or erase_tween:
+		#new anim
+		text_tween = create_tween()#.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_SINE)
+		#kill erasing animations if were switching from nothing
+		if erase_tween or text == "": 
+			if erase_tween: erase_tween.kill()
+			text_tween.tween_property(self,"visible_ratio",0,0)
+		#anim erase if switching from one to another
+		else: text_tween.tween_property(self,"visible_ratio",0,anim_time)
+		
+		
+		#switch the text
+		text_tween.tween_property(self,"text",interact_text,0)
+		
+		#retype the new text
+		anim_time = len(interact_text)/letters_per_sec
+		text_tween.tween_property(self,"visible_ratio",1,anim_time)
+			
+	#if looking at nothing
+	
+
+func clear_text() -> void:
+	interact_ray_active -=1
+	
+	if interact_ray_active == 0:
+		interact_text = ""
+		if text_tween: text_tween.kill()
+		erase_tween = create_tween()#.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_SINE)
+		
+		erase_tween.tween_property(self,"visible_ratio",0,anim_time)
+		erase_tween.tween_property(self,"text","",0)
+		erase_tween.tween_property(self,"visible_ratio",1,0)
+	
+	elif interact_ray_active < 0:
+		push_warning("negative amount of viewed stuff")
--- Scripts/managers/player_interact_cast.gd
+++ Scripts/managers/player_interact_cast.gd
@@ -1,17 +1,22 @@
 extends RayCast3D
 
-var candle_look = Node3D
+var candle_look: Node3D = null
 var break_chance: int = 10
-@onready var interact_text: Label = $"../../../2D/Interact_text"
+@onready var popup_label: Label = $"../../../2D/PopupLabel"
 
 func _physics_process(_delta: float) -> void:
 	
 	if get_collider() and get_collider().get_parent().name == "Candle":
-		interact_text.visible = true
+		if candle_look != get_collider().get_parent():
+			#popup_label.interact_text = "[E]: Light candle"
+			popup_label.add_text("[E]: Light candle")
 		candle_look = get_collider().get_parent()
+		
 	else:
+		if candle_look:
+			popup_label.clear_text()
 		candle_look = null
-		interact_text.visible = false
+		
 		
 func _unhandled_key_input(_event: InputEvent) -> void:
 	

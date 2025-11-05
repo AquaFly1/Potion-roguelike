96b85695e4bd8273d52f6b95c2b28c5e1e90ff9c
AquaFly1 <louis.emery@hotmail.com>
2025-11-05 17:00:30 -0500

    upgradable combos

--- Scripts/resources/Combo.gd
+++ Scripts/resources/Combo.gd
@@ -6,6 +6,7 @@ class_name Combo
 @export var tags_needed: Array[Tag]
 @export var combos_removed: Array[Combo]
 @export var effects = {}
+var upgrade = {}
 var eff_array: Array[float]
 
 func _to_string() -> String:
@@ -19,3 +20,16 @@ func effect_values() -> Array:
 		for i in effects.keys():
 			eff_array[Effect.find(i)] = effects[i]
 	return eff_array
+
+## defines the upgrade dictionary by halving each value of the base effects and rounding them up
+func def_upgrade():
+	for i in range(effects.size()):
+		var value = effects.values()[i]
+		value /= 2
+		ceil(value)
+		upgrade.values()[i] = float(value)
+
+## adds each value of each effect of the upgrade dictionary to the effect dictionary
+func upgrade_effetcs():
+	for i in range(effects.size()):
+		effects.values()[i] += upgrade.values()[i]

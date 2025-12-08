f171cc740db7cd8ed40fa27993e6f320c8f888a0
Ti-Die <ewm.hoover@gmail.com>
2025-12-07 20:10:49 -0500

    interactray wasnt working as expectedd now it is also church

--- entities/'player/'player_character/rays/player_interact_cast.gd
+++ entities/'player/'player_character/rays/player_interact_cast.gd
@@ -4,44 +4,32 @@ var chest_look: Node3D = null
 var candle_look: Node3D = null
 var break_chance: int = 10
 @export var popup_label: Label
-var node_ref := {} ##Dictionnary of Interacteables you are looking at. ex: looking at a candle -> node_ref["Candle"] = candleNode 
-
+@export var name_and_text: Dictionary[String,String]
+var prev_object: Node3D
 
 
 func _physics_process(_delta: float) -> void:
-	check_for("Candle", "[E]: Light candle")
-	check_for("Chest", "[E]: Open chest")
-	#if get_collider() and "Candle" in get_collider().get_parent().name:
-		#if candle_look != get_collider().get_parent():
-			##popup_label.interact_text = "[E]: Light candle"
-			#popup_label.add_text("[E]: Light candle")
-		#candle_look = get_collider().get_parent()
-		#
-	#else:
-		#if candle_look:
-			#popup_label.clear_text("[E]: Light candle")
-		#candle_look = null
-#
-	#if get_collider() and "Chest" in get_collider().name:
-		#if chest_look != get_collider():
-			##popup_label.interact_text = "[E]: Light candle"
-			#popup_label.add_text("[E]: Open chest")
-		#chest_look = get_collider()
-	#else:
-		#if chest_look:
-			#popup_label.clear_text("[E]: Open chest")
-		#chest_look = null
-
-func check_for(object_hint: String, text: String) -> void:
-	if get_collider() and object_hint in get_collider().get_parent().name:
-		if node_ref.get(object_hint) != get_collider().get_parent():
-			popup_label.add_text(text)
-		node_ref[object_hint] = get_collider().get_parent()
+	
+	if get_collider():
+		var valid := false
+		var object = get_collider().get_parent()
+		for i in name_and_text.keys():
+			if i in object.name:
+				if object != prev_object: popup_label.set_popup_text("")
+				else:	popup_label.set_popup_text(name_and_text[i])
+				valid = true
+				
+				prev_object = object
+				break
+		if not valid:
+			popup_label.set_popup_text("")
+		
 		
 	else:
-		if node_ref.get(object_hint):
-			popup_label.clear_text(text)
-			node_ref.erase(object_hint)
+		popup_label.set_popup_text("")
+	
+
+	
 
 func _unhandled_input(_event: InputEvent) -> void:
 	
@@ -53,12 +41,18 @@ func _unhandled_input(_event: InputEvent) -> void:
 			
 	
 	if Input.is_action_just_pressed("interact"):
-		if node_ref.get("Candle"):
-			if randi_range(1,break_chance) != break_chance:
-				node_ref.get("Candle").light_candle()
-			else:
-				pass
-				#alumette se brise animation
-		elif node_ref.get("Chest"): node_ref.get("Chest").open_chest()
+		var type := ""
+		var object : Node3D
+		if get_collider(): object = get_collider().get_parent()
+		if object:
+			for i in name_and_text.keys(): if i in object.name:
+				type = i
+				
+			match type:
+				"Candle":
+					if randi_range(1,break_chance) != break_chance:
+						object.light_candle()
+				"Chest":
+					object.open_chest()
 
 	
--- props/PopupLabel.gd
+++ props/PopupLabel.gd
@@ -1,60 +1,38 @@
 extends Label
 
-var interact_text: Array[String] = [""]
 var text_tween: Tween
 var erase_tween: Tween
-@onready var interact_ray_active: int = 0
 @export var letters_per_sec: float
 var anim_time: float = 20
 var is_exit: bool
 
-
-
-
-func add_text(input_text) -> void:
-	interact_text.append(input_text)
-		
-	#tally how many objects say theyre being viewed, value is basically always 0 or 1 but
-	#when the viewd object switches it might fuck up a bool if the exit call happens
-	#after teh new object's enter call
-	interact_ray_active += 1
-	
-	#basically as long as the text isnt already there, avoid erasing just to put the
-	#same thing again
-	visible = true
-	if text != interact_text[-1] or erase_tween:
-		#new anim
-		text_tween = create_tween()#.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_SINE)
-		#kill erasing animations if were switching from nothing
-		if erase_tween or text == "": 
-			if erase_tween: erase_tween.kill()
-			text_tween.tween_property(self,"visible_ratio",0,0)
-		#anim erase if switching from one to another
-		else: text_tween.tween_property(self,"visible_ratio",0,anim_time)
-		
-		
-		#switch the text
-		text_tween.tween_property(self,"text",interact_text[-1],0)
-		
-		#retype the new text
-		anim_time = len(interact_text[-1])/letters_per_sec
-		text_tween.tween_property(self,"visible_ratio",1,anim_time)
+func set_popup_text(input_text) -> void:
+	if text != input_text and not (erase_tween and erase_tween.is_running()):
+		if input_text == "":
 			
-	#if looking at nothing
-	
-
-func clear_text(input_text) -> void:
-	interact_ray_active -=1
-	interact_text.erase(input_text)
-	
-	if interact_ray_active == 0:
-		if text_tween: text_tween.kill()
-		erase_tween = create_tween()#.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_SINE)
-		
-		erase_tween.tween_property(self,"visible_ratio",0,anim_time)
-		erase_tween.tween_property(self,"text","",0)
-		erase_tween.tween_property(self,"visible",false,0)
-		erase_tween.tween_property(self,"visible_ratio",1,0)
-	
-	elif interact_ray_active < 0:
-		push_warning("negative amount of viewed stuff")
+			if text_tween: text_tween.kill()
+			erase_tween = create_tween()#.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_SINE)
+				
+			erase_tween.tween_property(self,"visible_ratio",0,anim_time)
+			erase_tween.tween_property(self,"text","",0)
+			erase_tween.tween_property(self,"visible",false,0)
+			erase_tween.tween_property(self,"visible_ratio",1,0)
+			
+			
+		else:
+			#new anim
+			text_tween = create_tween()#.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_SINE)
+			#kill erasing animations if were switching from nothing
+			if erase_tween or text == "": 
+				if erase_tween: erase_tween.kill()
+				text_tween.tween_property(self,"visible_ratio",0,0)
+			#anim erase if switching from one to another
+			else: text_tween.tween_property(self,"visible_ratio",0,anim_time)
+			
+				#switch the text
+			text_tween.tween_property(self,"text",input_text,0)
+			visible = true
+			#retype the new text
+			anim_time = len(input_text)/letters_per_sec
+			text_tween.tween_property(self,"visible_ratio",1,anim_time)
+			
